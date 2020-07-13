0fb8e744a4d132b03dfe9b3d2a2b329f683e8af6
Yosra <yos.skhiri@gmail.com>
2020-07-13 15:28:40 +0100

    made the tabs in the product page functional

--- build/scripts/product-page.js
+++ build/scripts/product-page.js
@@ -1,8 +1,34 @@
 let images = document.querySelectorAll('.product-display__side-image > img');
 let mainProductImage = document.querySelector('.product-display__main-image > img');
 
+let btnDescription = document.querySelector('#product-description-btn');
+let btnReviews = document.querySelector('#product-reviews-btn');
+
+let tabDescription = document.querySelector('#product-description');
+let tabReviews = document.querySelector('#product-reviews');
+
+
 images.forEach(img => {
     img.addEventListener('click', ()=> {
         mainProductImage.src = img.src;
     })
+})
+
+btnDescription.addEventListener('click', () => {
+    tabDescription.style='display: flex;';
+    tabReviews.style='display: none;';
+
+    btnDescription.classList.remove('btn__tab--inactive');
+    btnDescription.classList.add('btn__tab--active');
+    btnReviews.classList.add('btn__tab--inactive');
+
+})
+
+btnReviews.addEventListener('click', () => {
+    tabReviews.style='display: grid;';
+    tabDescription.style='display: none;';
+
+    btnReviews.classList.remove('btn__tab--inactive');
+    btnReviews.classList.add('btn__tab--active');
+    btnDescription.classList.add('btn__tab--inactive');
 })
