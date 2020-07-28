10eef7cf154239109f3b6d4dccd3d25777b0b673
Yosra <yos.skhiri@gmail.com>
2020-07-28 12:30:45 +0100

    added the animations to all the filters in the search page

--- build/scripts/filter.js
+++ build/scripts/filter.js
@@ -1,7 +1,20 @@
-let productBtn = document.querySelector('#product-typ-btn')
-let productTab = document.querySelector('#product-tab')
+let productBtn = document.querySelectorAll('#product-typ-btn');
+let productTab = document.querySelectorAll('#product-tab');
+let productTabArrows = document.querySelectorAll('.filter-title-arrow');
 
-productBtn.addEventListener('click', () => {
-    productTab.classList.toggle('filter-toggle');
-})
-
+productBtn.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        productTabArrows[index].classList.toggle('rotate');
+        if(productTab[index].style.display === "none"){
+            productTab[index].style.display = "block";
+            setTimeout(function() {
+                productTab[index].classList.toggle('filter-toggle');
+            }, 100);
+        } else {
+            productTab[index].classList.toggle('filter-toggle');
+            setTimeout(function() {
+                productTab[index].style.display = "none";
+            }, 500);
+        }
+    });
+});
